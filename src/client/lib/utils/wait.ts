06a9879b320139fc6118377d58423511caad91f7
Primordial-Devv <primordial.devv@gmail.com>
2026-03-29 13:53:31 +0200

    FEAT - Adding wait client side system to waint until a certain event or to wait for payer to connect on the server

--- src/client/lib/utils/wait.ts
+++ src/client/lib/utils/wait.ts
@@ -1,7 +1,3 @@
-export function wait(ms: number): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 export function waitUntil(condition: () => boolean, interval = 0, timeout = 30000): Promise<void> {
   return new Promise((resolve, reject) => {
     const startTime = Date.now();
@@ -37,14 +33,4 @@ export function waitUntil(condition: () => boolean, interval = 0, timeout = 3000
 
 export function waitForPlayer(timeout = 30000): Promise<void> {
   return waitUntil(() => NetworkIsPlayerActive(PlayerId()), 0, timeout);
-}
-
-export function waitForModel(model: number, timeout = 10000): Promise<void> {
-  RequestModel(model);
-  return waitUntil(() => HasModelLoaded(model), 0, timeout);
-}
-
-export function waitForAnimDict(dict: string, timeout = 10000): Promise<void> {
-  RequestAnimDict(dict);
-  return waitUntil(() => HasAnimDictLoaded(dict), 0, timeout);
 }
