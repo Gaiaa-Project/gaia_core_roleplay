88016ea8c5bc6199c1bcd964b47bfdb2da215a16
Primordial-Devv <primordial.devv@gmail.com>
2026-03-30 13:17:03 +0200

    UPDATE - Updating the entry point to all client new feature

--- src/client/index.ts
+++ src/client/index.ts
@@ -18,6 +18,41 @@ import {
 } from './lib/streaming/request';
 import { raycastFromCoords, raycastFromCamera, raycastFromEntity } from './lib/raycast/main';
 import { addKeybind, removeKeybind, enableKeybind, isKeybindPressed } from './lib/keybind/main';
+import {
+  getClosestPlayer as getClosestPlayerClient,
+  getNearbyPlayers as getNearbyPlayersClient,
+} from './lib/player/main';
+import {
+  addDisabledControl,
+  removeDisabledControl,
+  clearDisabledControl,
+  clearAllDisabledControls,
+  suppressDisabledControl,
+  unsuppressDisabledControl,
+  hasDisabledControl,
+  isDisabledControlSuppressed,
+  getDisabledControlCallers,
+  getAllDisabledControls,
+  tickDisabledControls,
+} from './lib/controls/main';
+import {
+  addPoint,
+  removePoint,
+  getClosestPoint,
+  getPointsInside,
+  getPointsNearby,
+} from './lib/spatial/points';
+import {
+  addClientSphereZone,
+  addClientBoxZone,
+  addClientPolyZone,
+  removeClientZone,
+  isInsideZone,
+  getCurrentZones,
+} from './lib/spatial/zones';
+import { getVehicleProperties, setVehicleProperties } from './lib/vehicle/properties';
+import './lib/vehicle/init';
+import './modules/world_adjustments';
 
 exports('TriggerServerCallback', TriggerServerCallback);
 exports('RegisterClientCallback', RegisterClientCallback);
@@ -39,10 +74,41 @@ exports('raycastFromCoords', raycastFromCoords);
 exports('raycastFromCamera', raycastFromCamera);
 exports('raycastFromEntity', raycastFromEntity);
 
+exports('getClosestPlayer', getClosestPlayerClient);
+exports('getNearbyPlayers', getNearbyPlayersClient);
+
 exports('addKeybind', addKeybind);
 exports('removeKeybind', removeKeybind);
 exports('enableKeybind', enableKeybind);
 exports('isKeybindPressed', isKeybindPressed);
+
+exports('addDisabledControl', addDisabledControl);
+exports('removeDisabledControl', removeDisabledControl);
+exports('clearDisabledControl', clearDisabledControl);
+exports('clearAllDisabledControls', clearAllDisabledControls);
+exports('suppressDisabledControl', suppressDisabledControl);
+exports('unsuppressDisabledControl', unsuppressDisabledControl);
+exports('hasDisabledControl', hasDisabledControl);
+exports('isDisabledControlSuppressed', isDisabledControlSuppressed);
+exports('getDisabledControlCallers', getDisabledControlCallers);
+exports('getAllDisabledControls', getAllDisabledControls);
+exports('tickDisabledControls', tickDisabledControls);
+
+exports('addPoint', addPoint);
+exports('removePoint', removePoint);
+exports('getClosestPoint', getClosestPoint);
+exports('getPointsInside', getPointsInside);
+exports('getPointsNearby', getPointsNearby);
+
+exports('addClientSphereZone', addClientSphereZone);
+exports('addClientBoxZone', addClientBoxZone);
+exports('addClientPolyZone', addClientPolyZone);
+exports('removeClientZone', removeClientZone);
+exports('isInsideZone', isInsideZone);
+exports('getCurrentZones', getCurrentZones);
+
+exports('getVehicleProperties', getVehicleProperties);
+exports('setVehicleProperties', setVehicleProperties);
 
 const log = Print.create('Core');
 
